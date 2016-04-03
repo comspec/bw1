394ac4d32b744e01658b8d912b963963fefbfa34
scopolamin <scopolamin@gmail.com>
2016-04-03 22:43:17 +0200

    Videoplayer korrigiert (neu mit Pause)

--- js/bw1.js
+++ js/bw1.js
@@ -1,18 +1,32 @@
 $(function() {
-	$(".modalDialog").prop("opacity", 0.2);
+	var modal = $(".modalDialog");
+	var player = $("#player");
 
 	$(".previewbox").on("click", "img", function(e) {
 		e.preventDefault();
 
-		var player = $("#player");
-		player.prop("src", this.src.replace("jpg", "mp4"));
-		player.get(0).load();
-		player.get(0).play();
+		if (modal.css("opacity") === "0") {
 
-		//$(".modalDialog").prop("opacity", 1);
-		//$(".modalDialog").addClass("modalDialog");
-		//$(".modalDialog").addClass("video");
-		//$(".modalDialog").prop("visibility", "visible");
+			if (player.get(0).paused === true && player.prop("src") === this.src.replace("jpg", "mp4")) {
+				player.get(0).play();
+				modal.css("opacity", 1);
+			} else {
+				player.prop("src", this.src.replace("jpg", "mp4"));
+				player.get(0).load();
+				player.get(0).play();
+
+				modal.css("opacity", 1);
+			}
+		}
+	});
+
+	$(document).on('click', function(e){
+		e.preventDefault();
+
+        if (modal.css("opacity") === "1" && player.get(0).paused === false) {
+        	player.get(0).pause();
+			modal.css("opacity", 0);
+        }
 	});
 
 	$("#player").on("timeupdate", function(e) {
