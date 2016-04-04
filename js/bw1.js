0a120e5f65a981ed8c412204c022ba4c697a9363
scopolamin <scopolamin@gmail.com>
2016-04-04 20:13:20 +0200

    Videoliste dynamisch aus JSON-Definition generieren.

--- js/bw1.js
+++ js/bw1.js
@@ -1,17 +1,24 @@
 $(function() {
 	var modal = $(".modalDialog");
 	var player = $("#player");
-
+	
+	$.getJSON("videos.json", function (data) {
+        $.each(data, function (key, obj) {
+            console.log(key, obj)
+            $("<div class='col-sx-6 col-sm-6 col-md-3 col-lg-4'><div class='panel previewbox'><img class='previewimage' src='" + obj.ImageURL + "'></img><span>" + obj.Name + "</span></div></div>").appendTo(".container");
+        });
+    });
+	
 	$(".previewbox").on("click", "img", function(e) {
 		e.preventDefault();
 
 		if (modal.css("opacity") === "0") {
 
-			if (player.get(0).paused === true && player.prop("src") === this.src.replace("jpg", "mp4")) {
+			if (player.get(0).paused === true && player.prop("src") === this.src.replace("png", "mp4")) {
 				player.get(0).play();
 				modal.css("opacity", 1);
 			} else {
-				player.prop("src", this.src.replace("jpg", "mp4"));
+				player.prop("src", this.src.replace("png", "mp4"));
 				player.get(0).load();
 				player.get(0).play();
 
@@ -29,7 +36,7 @@ $(function() {
         }
 	});
 
-	$("#player").on("timeupdate", function(e) {
+	/*$("#player").on("timeupdate", function(e) {
       console.log(this.currentTime + " / " + this.duration);
-    });
+    });*/
 });
